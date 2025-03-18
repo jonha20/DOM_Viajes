1cf4eb3bca7443133d66e03925fb1336df6fb501
jonha20 <64472507+jonha20@users.noreply.github.com>
2025-03-18 11:29:07 +0100

    Ejercicio arreglado

--- js/script.js
+++ js/script.js
@@ -21,37 +21,37 @@ const data = [
 ];
 const dataViajes = [
   {
-    title: "Example Title 1",
+    title: "Viaje 1",
     description: "Example Description 1",
     url_img: "./assets/viajes/viajes-1.jpg",
   },
   {
-    title: "Example Title 2",
+    title: "Viaje 2",
     description: "Example Description 2",
     url_img: "./assets/viajes/viajes-2.jpg",
   },
   {
-    title: "Example Title 3",
+    title: "Viaje 3",
     description: "Example Description 3",
     url_img: "./assets/viajes/viajes-3.jpg",
   },
   {
-    title: "Example Title 3",
+    title: "Viaje 4",
     description: "Example Description 3",
     url_img: "./assets/viajes/viajes-4.jpg",
   },
   {
-    title: "Example Title 3",
+    title: "Viaje 5",
     description: "Example Description 3",
     url_img: "./assets/viajes/viajes-5.jpg",
   },
   {
-    title: "Example Title 3",
+    title: "Viaje 6",
     description: "Example Description 3",
     url_img: "./assets/viajes/viajes-6.jpg",
   },
   {
-    title: "Example Title 3",
+    title: "Viaje 7",
     description: "Example Description 3",
     url_img: "./assets/viajes/viajes-7.jpg",
   },
@@ -72,9 +72,26 @@ const cities = [
 document
   .querySelectorAll("#contenedor1 > figure > img")[0]
   .setAttribute("src", data[1].url_img);
-document.querySelectorAll("#contenedor2 div img").forEach((fotos) => {
-  fotos.setAttribute("src", getRandomIndex(dataViajes).url_img);
+
+
+let contenedor = document.getElementById("contenedor2")
+dataViajes.forEach((viajes)=>{
+  contenedor.innerHTML += 
+        `<div class="viaje">
+          <img src="${viajes.url_img}" alt="" srcset="" />
+          <div class="viaje-texto">
+            <h4>${viajes.title}</h4>
+            <p>
+              Lorem Ipsum es simplemente el texto de relleno de las imprentas y
+              archivos de texto. Lorem Ipsum ha sido el texto de relleno
+              estándar de las industrias desde el año 1500, cuando un impresor
+              (N. del T. persona que se dedica a la imprenta) desconocido usó
+              una galería de textos y los mezcló de tal manera que logró hacer
+            </p>
+          </div>
+        </div>`
 });
+  
 let destinos = document.getElementById("comunidades");
 cities.forEach((city) => {
   destinos.innerHTML += `<option value="${city}">${city}</option>`;
